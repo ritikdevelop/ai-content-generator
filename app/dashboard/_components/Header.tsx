61489684aeea6b02160ffd57c1f433ae2f8c7ffc
ritikdevelop <hrithikrana216@gmail.com>
2024-10-14 11:15:50 +0530

    Fourth commit in AI Content Generator app

--- app/dashboard/_components/Header.tsx
+++ app/dashboard/_components/Header.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Header() {
   return (
     <div className='p-5 shadow-sm border-b-2 flex justify-between items-center'>
-      <div className='flex gap-2 items-center p-2 border rounded-md max-w-lg'>
+      <div className='flex gap-2 items-center p-2 border rounded-md max-w-lg bg-white'>
         <Search />
         <input type='text' placeholder='Search..' className='outline-none' />
       </div>
--- app/dashboard/content/[template-slug]/page.tsx
+++ app/dashboard/content/[template-slug]/page.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import FormSection from "../_components/FormSection";
 import OutputSection from "../_components/OutputSection";
 import { TEMPLATE } from "../../_components/TemplateListSection";
 import Templates from "@/app/(data)/Templates";
+import { Button } from "@/components/ui/button";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+import { chatSession } from "@/utils/AiModal";
 
 interface PROPS {
   params: {
@@ -15,12 +20,42 @@ function CreateNewContent(props: PROPS) {
     (item) => item.slug == props.params["template-slug"]
   );
 
+  const [loading, setLoading] = useState(false);
+  const [aiOutput, setAiOutput] = useState<string>("");
+  const GenerateAIContent = async (formData: any) => {
+    setLoading(true);
+    // Your logic here to generate AI content based on the formData
+    const SelectedPrompt = selectedTemplate?.aiPrompt;
+
+    const FinalAIPrompt = JSON.stringify(formData) + ", " + SelectedPrompt;
+
+    const result = await chatSession.sendMessage(FinalAIPrompt);
+
+    console.log(result.response.text());
+
+    setAiOutput(result?.response.text());
+    setLoading(false);
+  };
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-5 p-5">
-      {/* Form Section */}
-      <FormSection selectedTemplate={selectedTemplate} />
-      {/* Output Section */}
-      <OutputSection />
+    <div className="p-2">
+      <Link href={"/dashboard"}>
+        <Button>
+          <ArrowLeft /> Back
+        </Button>
+      </Link>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 py-2">
+        {/* Form Section */}
+        <FormSection
+          selectedTemplate={selectedTemplate}
+          userFormInput={(v: any) => GenerateAIContent(v)}
+          loading={loading}
+        />
+        {/* Output Section */}
+        <div className="col-span-2">
+          <OutputSection aiOutput={aiOutput} />
+        </div>
+      </div>
     </div>
   );
 }
