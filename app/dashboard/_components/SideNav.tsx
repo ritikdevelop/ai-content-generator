925efbb08b882ac52a74530724227fb19fc846e2
ritikdevelop <hrithikrana216@gmail.com>
2024-12-25 15:22:52 +0530

    Add UsageTrack component and update SideNav; rename settings path and remove redundant setting page

--- app/dashboard/_components/SideNav.tsx
+++ app/dashboard/_components/SideNav.tsx
@@ -3,6 +3,7 @@ import { FileClock, Home, Settings, WalletCards } from "lucide-react";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
 import React, { useEffect } from "react";
+import UsageTrack from "./UsageTrack";
 
 function SideNav() {
   const MenuList = [
@@ -24,7 +25,7 @@ function SideNav() {
     {
       name: "Setting",
       icon: Settings,
-      path: "/dashboard/setting",
+      path: "/dashboard/settings",
     },
   ];
 
@@ -34,7 +35,7 @@ function SideNav() {
   }, []);
 
   return (
-    <div className="h-screen p-5 shadow-sm border">
+    <div className="h-screen relative p-5 shadow-sm border">
       <div className="flex justify-center ">
         <Image src={"/logo.svg"} width={120} height={100} alt={"Logo"} />
       </div>
@@ -42,6 +43,7 @@ function SideNav() {
       <div className="mt-3">
         {MenuList.map((menu, index) => (
           <div
+            key={index}
             className={`flex gap-2 mb-2 p-3 hover:bg-primary hover:text-white rounded-lg cursor-pointer items-center ${
               path == menu.path && "bg-primary text-white"
             }`}
@@ -50,6 +52,10 @@ function SideNav() {
             <h2 className="text-lg">{menu.name}</h2>
           </div>
         ))}
+      </div>
+
+      <div className="absolute bottom-10 w-full left-0">
+        <UsageTrack />
       </div>
     </div>
   );
